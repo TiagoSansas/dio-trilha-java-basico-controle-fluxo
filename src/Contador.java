43ea493c67c90a41fd820dc8daeed0cf0a9186fc
Tiago Souza <tiagoeduardosouza@gmail.com>
2024-03-08 14:35:27 -0300

    Recebendo dados do terminal

---
+++ src/Contador.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Contador {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Digite o primeiro parâmetro");
+    int parametroUm = sc.nextInt();
+
+    System.out.println("Digite o segundo parâmetro");
+    int parametroDois = sc.nextInt();
+  }
+}
